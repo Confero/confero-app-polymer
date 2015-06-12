ef957101ce9e2a818be5f993bffc6806acff04a3
Rylan Cottrell <rylan@icottrell.com>
2015-06-11 20:57:27 -0400

    updated wct file

--- wct.conf.js
+++ wct.conf.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: 'app',
-  suites: ['test']
+  suites: ['test'],
+   plugins: {
+        sauce: {
+            browsers: [
+                "Windows 8.1/internet explorer",
+                "Windows 8.1/chrome",
+                "Windows 8.1/firefox",
+                "OS X 10.10/chrome",
+                "OS X 10.10/firefox",
+                "OS X 10.10/safari"
+            ]
+        }
+    }
 };
