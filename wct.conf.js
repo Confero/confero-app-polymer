47739dcfd123f7ae5f6872a78cf0f7bc50ecbc47
Rylan Cottrell <rylan@icottrell.com>
2015-06-11 21:09:41 -0400

    update config

--- wct.conf.js
+++ wct.conf.js
@@ -7,8 +7,6 @@ module.exports = {
                 "Windows 8.1/internet explorer",
                 "Windows 8.1/chrome",
                 "Windows 8.1/firefox",
-                "OS X 10.10/chrome",
-                "OS X 10.10/firefox",
                 "OS X 10.10/safari"
             ]
         }
